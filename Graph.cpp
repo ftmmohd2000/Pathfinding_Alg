1347d66948d02c60448d0c6dcb19a2fd55ca4fd2
ftmmohd <ftmmohd2000@gmail.com>
2020-01-20 17:33:13 -0500

    Integration works, few errors in removeNode()

--- Graph.cpp
+++ Graph.cpp
@@ -1,32 +1,85 @@
+#include <iostream>
 #include "Graph.h"
+#include "NodeObject.h"
 
-Graph<class T>::Graph(){
-    Graph::graphList = new std::vector<T*>();
+Graph::Graph(){
+    Graph::graphList = new std::vector<NodeObject*>();
     Graph::refList = new std::vector<int>();
     Graph::pathfinder = NULL;
 }
 
-Graph<class T>::Graph(std::vector<T*>*(*myAlg)(Graph*,T*,T*)){
-    Graph::graphList = new std::vector<T*>();
+Graph::Graph(std::vector<NodeObject*>(*myAlg)(Graph*,NodeObject*,NodeObject*)){
+    Graph::graphList = new std::vector<NodeObject*>();
     Graph::refList = new std::vector<int>();
     Graph::pathfinder = myAlg;
 }
 
-int Graph<class T>::getSize(){
+int Graph::getSize(){
     return Graph::graphList->size();
 }
 
-bool Graph<class T>::addNode(T *node,int id){
+bool Graph::addNode(NodeObject *node,int id){
 
     for(std::vector<int>::iterator i = Graph::refList->begin();i!=Graph::refList->end();++i){
-        if((*i)->getID() == id)
+        if((*i) == id)
             return false;
     }
 
+    Graph::refList->push_back(id);
+    Graph::graphList->push_back(node);
 
+    return true;
 }
 
-Graph<class T,class S>::~Graph(){
+bool Graph::removeNode(NodeObject *node){
+
+    int id = node->getID();
+
+    for(std::vector<NodeObject*>::iterator i = Graph::graphList->begin();i!=Graph::graphList->end();++i){
+        if(*i == node){
+            Graph::graphList->erase(i);
+        }
+    }
+    for(std::vector<int>::iterator i = Graph::refList->begin();i!=Graph::refList->end();++i){
+        if(*i == id){
+            Graph::refList->erase(i);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void Graph::printGraph(){
+
+    std::vector<NodeObject*> tempArr;
+
+    for(std::vector<NodeObject*>::iterator i = Graph::graphList->begin();i!=Graph::graphList->end();++i){
+        tempArr = ((*i)->getCons());
+
+        std::cout<<"Node ID "<<(*i)->getID()<<" connects to: ";
+
+        for(std::vector<NodeObject*>::iterator j = tempArr.begin();j!=tempArr.end();++j)
+            std::cout<<(*j)->getID()<<" ";
+        
+        std::cout<<std::endl;
+    }
+}
+
+NodeObject* Graph::getNode(int id){
+    for(std::vector<NodeObject*>::iterator i = Graph::graphList->begin();i!=Graph::graphList->end();++i){
+        if((*i)->getID() == id)
+            return *i;
+    }
+
+    return NULL;
+}
+
+std::vector<NodeObject*> Graph::findPath(Graph *g,NodeObject *start, NodeObject *target){
+    return Graph::pathfinder(g,start,target);
+}
+
+Graph::~Graph(){
     delete graphList;
     delete refList;
 }
--- main.cpp
+++ main.cpp
@@ -5,8 +5,22 @@
 int main(){
     NodeObject myNode1(5);
     myNode1.setSeen(true);
-    Graph<NodeObject,int> mygraph;
-    std::cout << myNode1.hasBeenSeen() << std::endl;
+    Graph *myGraph = new Graph();
+
+    myGraph->printGraph();
+    
+    myGraph->addNode(&myNode1,myNode1.getID());
+    NodeObject myNode2(89);
+    myNode2.joinTo(&myNode1);
+
+    myGraph->addNode(&myNode2,myNode2.getID());
+
+    myGraph->printGraph();
+
+    //myGraph->removeNode(&myNode2);
+
+    myGraph->printGraph();
+    //std::cout << myNode1.hasBeenSeen() << std::endl;
 
     return 0;
 }
